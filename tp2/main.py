94fe2056242dcd5c2c606ceb21ccf35124a15703
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-04-06 08:12:14 -0400

    Add the State class

--- tp2/main.py
+++ tp2/main.py
@@ -2,6 +2,54 @@
 
 from pathlib import Path
 
+class State(object):
+    """ Constructeur.
+
+        :param state: Le nom de l'état.
+    """
+    def __init__(self, state):
+        self.__state = state
+        self.__transitions = dict()
+
+    """ Cette fonction ajoute une transition à l'état.
+
+        :param symbol: L'entrée à la transition.
+        :param nstate: Le prochain état.
+    """
+    def add_transition(self, symbol, nstate):
+        # on ajoute la transition à la table
+        self.__transitions[symbol] = nstate
+
+    """ Cette fonction obtient le prochain état selon une entrée.
+
+        :param symbol: L'entrée pour la transition.
+
+        :return: Le prochain état si la transition existe, sinon None.
+    """
+    def next(self, symbol):
+        # on s'assure que la transition existe
+        if not symbol in self.__transitions:
+            return None
+
+        # on retourne le prochain état
+        return self.__transitions[symbol]
+
+    """ Cette fonction détermine si un état est un état final.
+
+        :return: True si c'est un état final, sinon False.
+    """
+    def is_terminal(self):
+        # aucune transition implique que c'est un état final
+        return (len(self.__transitions) == 0)
+        
+
+    """ Cette fonction retourne le nom de l'état.
+
+        :return: Le nom de l'état.
+    """
+    def get_state(self):
+        return self.__state
+
 class StateMachine(object):
     def __init__(self, numero, states):
         self.__numero = numero
