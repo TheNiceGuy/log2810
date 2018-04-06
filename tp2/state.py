e80d9a2a6d5c415815ff6ebd2ecf0bfb3bb98632
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-04-06 08:18:39 -0400

    Move State class into its own file

---
+++ tp2/state.py
@@ -0,0 +1,50 @@
+class State(object):
+    """ Constructeur.
+
+        :param state: Le nom de l'état.
+    """
+    def __init__(self, state):
+        self.__state = state
+        self.__transitions = dict()
+
+    """ Cette fonction ajoute une transition à l'état.
+
+        :param symbol: L'entrée à la transition.
+        :param nstate: Le prochain état.
+    """
+    def add_transition(self, symbol, nstate):
+        # on s'assure que la transition n'existe pas déjà
+        if symbol in self.__transitions:
+            raise ValueException()
+
+        # on ajoute la transition à la table
+        self.__transitions[symbol] = nstate
+
+    """ Cette fonction obtient le prochain état selon une entrée.
+
+        :param symbol: L'entrée pour la transition.
+
+        :return: Le prochain état si la transition existe, sinon None.
+    """
+    def next(self, symbol):
+        # on s'assure que la transition existe
+        if not symbol in self.__transitions:
+            return None
+
+        # on retourne le prochain état
+        return self.__transitions[symbol]
+
+    """ Cette fonction détermine si un état est un état final.
+
+        :return: True si c'est un état final, sinon False.
+    """
+    def is_terminal(self):
+        # aucune transition implique que c'est un état final
+        return (len(self.__transitions) == 0)
+        
+
+    """ Cette fonction retourne le nom de l'état.
+
+        :return: Le nom de l'état.
+    """
+    def get_state(self):
